4f1341b745b5c8b7f256c55f7fc9b8d32276c8e4
damndaniel088 <ciaodanel@gmail.com>
2025-12-07 15:43:37 +1100

    feat: incorrect approach to part 2, but best i have for now. need to consider indents somehow

--- days/day6/day6.py
+++ days/day6/day6.py
@@ -28,3 +28,54 @@ for column in columns:
         total_sum += num1 * num2 * num3 * num4
 
 print('part 1', total_sum)
+
+
+#------- part 2 -------
+
+# reset sum counter
+total_sum = 0
+
+# same basic structure as part 1
+for column in columns:
+    operator = column[-1]
+    
+    num1_original = int(column[0])
+    num2_original = int(column[1])
+    num3_original = int(column[2])
+    num4_original = int(column[3])
+    print(column)
+
+    # Don't use zfill - work with original strings
+    num1_str = str(num1_original)
+    num2_str = str(num2_original)
+    num3_str = str(num3_original)
+    num4_str = str(num4_original)
+
+    # Get max length to know how many columns
+    max_len = max(len(num1_str), len(num2_str), len(num3_str), len(num4_str))
+
+    # Read right-to-left positions
+    new_nums = []
+    for pos in range(max_len):
+        digits = ''
+        for num_str in [num1_str, num2_str, num3_str, num4_str]:
+            if len(num_str) > pos:
+                digits += num_str[-(pos+1)]  # Read from right
+        if digits:
+            new_nums.append(int(digits))
+
+    new_nums.reverse()  # We built them backwards, so reverse
+
+    for num in new_nums:
+        print(num)
+
+    # apply the operator
+    if operator == '+':
+        total_sum += sum(new_nums)
+    elif operator == '*':
+        result = 1
+        for num in new_nums:
+            result *= num
+        total_sum += result
+
+print('part 2', total_sum)
